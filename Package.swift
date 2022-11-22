1de06c6ca1e593c20953f25911fcab3a55ada155
Jan Sallads <jan@fddb.de>
2022-11-22 02:41:15 +0100

    - fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "QTools",
-            targets: ["QTools"]),
+            targets: ["CachedAsyncImage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +18,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "QTools",
-            dependencies: []),
-        .testTarget(
-            name: "QToolsTests",
-            dependencies: ["QTools"]),
+//        .target(
+//            name: "QTools",
+//            dependencies: []),
+//        .testTarget(
+//            name: "QToolsTests",
+//            dependencies: ["QTools"]),
         
         .target(name: "CachedAsyncImage"),
     ]
